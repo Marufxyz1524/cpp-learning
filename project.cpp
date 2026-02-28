575481189e7ddc601d01e26b34e2cc70d48eded8
Marufxyz1524 <marufxyz1524@gmail.com>
2026-02-28 23:37:57 +0600

    Upload with new code

--- project.cpp
+++ project.cpp
@@ -3,13 +3,15 @@ using namespace std;
 
 int main()
 {
-    int n = 4;
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
     for (int i = 1; i <= n; i++)
     {
-
-        for (int j = 1; j <= n; j++)
+        for (int j = 1; j <= i; j++)
         {
-            cout << j << " ";
+            cout << "*";
         }
         cout << endl;
     }
