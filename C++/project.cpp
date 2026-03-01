c08dc3d0606b801234c8f29ee29919b8aa9a6774
Marufxyz1524 <marufxyz1524@gmail.com>
2026-03-01 15:22:16 +0600

    Same numbr triangle pattrn

--- C++/project.cpp
+++ C++/project.cpp
@@ -47,12 +47,23 @@ int main()
     }
     return 0;
     */
-    int n = 10;
+    //--TRIANGLE MADE BY ***  --->>>
+    /*int n = 10;
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j < i + 1; j++)
         {
             cout << "*";
+        }
+        cout << endl;
+    }*/
+
+    int n = 4;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < (i + 1); j++)
+        {
+            cout << (i + 1);
         }
         cout << endl;
     }
