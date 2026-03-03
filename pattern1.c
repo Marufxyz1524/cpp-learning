c4191b052c3fbdc15ac68fbea556ca2d151021c7
Marufxyz1524 <marufxyz1524@gmail.com>
2026-03-03 19:37:38 +0600

    when binary number output pattern

--- pattern1.c
+++ pattern1.c
@@ -3,11 +3,11 @@
 int main()
 {
     int n = 4;
-    for (int i = 1; i <= 4; i++)
+    for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; j <= n; j++)
+        for (int j = 1; j <= i; j++)
         {
-            printf("%d  ", j);
+            printf("%d ", j % 2);
         }
         printf("\n");
     }
