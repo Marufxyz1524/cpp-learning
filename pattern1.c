58b01ad15dcf0f64c43ed406acc97d5262ee92f7
Marufxyz1524 <marufxyz1524@gmail.com>
2026-03-04 22:41:08 +0600

    Sombaho triangle

--- pattern1.c
+++ pattern1.c
@@ -5,9 +5,18 @@ int main()
     /*
     [  123
        12
-       1   ]
+       1   ]*/
     int n = 4;
-    for (int i = n; i >= 1; i--)
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    for (int i = n - 1; i >= 1; i--)
     {
         for (int j = 1; j <= i; j++)
         {
@@ -17,5 +26,4 @@ int main()
     }
 
     return 0;
-    */
 }
