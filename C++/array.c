91ae98cbf7cfad600f642cc3d5154825d4500b20
Marufxyz1524 <marufxyz1524@gmail.com>
2026-03-07 22:26:27 +0600

    Array print and scanf

---
+++ C++/array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{ /*
+  //ARRAY USER INPUT
+  int marks[10];
+  for (int i = 0; i <= 3; i++)
+  {
+      scanf("%d", &marks[i]);  //%D ER POR KOKHONO SPACE DEOA JABENA
+  }
+*/
+
+    // PRINTING ARRAY---
+    int marks[5] = {55, 45, 54, 54};
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("The marks is = %d\n", marks[3]);
+    }
+
+    return 0;
+}
