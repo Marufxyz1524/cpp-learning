63cbe9b4392ea43f108071b33e7ed5fa17891f0b
Marufxyz1524 <marufxyz1524@gmail.com>
2026-03-30 15:49:33 +0600

    arrray

---
+++ C++/ARRAY PART1.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a[3], sum = 0, i;
+    scanf("%d %d %d", &a[0], &a[1], &a[2]);
+    for (int i = 0; i < 3; i++)
+    {
+        sum = sum + a[i];
+    }
+    printf("The sum is = %d", sum);
+
+    return 0;
+}
--- C++/array.c
+++ C++/array.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main()
-{ /*
+{
+    /*
   //ARRAY USER INPUT
   int marks[10];
   for (int i = 0; i <= 3; i++)
@@ -11,12 +12,32 @@ int main()
 */
 
     // PRINTING ARRAY---
-    int marks[5] = {55, 45, 54, 54};
+    /*int marks[5] = {55, 45, 54, 54};
 
     for (int i = 0; i < 4; i++)
     {
         printf("The marks is = %d\n", marks[3]);
     }
+        */
+    int num[100], n;
+    printf("How many numbers: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i < n; i++)
+    {
+
+        printf("Enter Number is %d : ", i);
+        scanf("%d", &num[i]);
+    }
+    int max = num[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (max < num[i])
+        {
+            max = num[i];
+        }
+    }
+    printf("The maxmimun value is : %d\n", max);
 
     return 0;
 }
