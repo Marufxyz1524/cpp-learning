84826e384bd8d46a891b13ec5d5c1ab971d50a97
Marufxyz1524 <marufxyz1524@gmail.com>
2026-04-01 12:44:23 +0600

    Copying array

---
+++ C++/ArrayToArryCopy.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+
+    int a[5] = {1, 2, 3, 4, 5};
+    int b[5];
+    for (int i = 0; i < 5; i++)
+    {
+        b[i] = a[i];
+    }
+    printf("new aray");
+    for (int i = 0; i < 5; i++)
+    {
+
+                printf("%d ", b[i]);
+    }
+
+    return 0;
+}
